9421f8e79f33ae1b81d2b47194ea635b33360b17
dmitry0792 <89214075008@mail.ru>
2021-12-12 21:32:35 +0300

    for>forEach, new function name

--- converters.js
+++ converters.js
@@ -1,43 +1,40 @@
+/*   Конверторы из двоичной в десятичную систему счисления (и наоборот),
+     на данном этапе работает только с целыми числами.                     */
 
-/*   Конвертор из двоичной в десятичную систему счисления (и наоборот),
-     на данный момент работает только с целыми числами.                             */
+// Из двоичной
 
-
-const twoToTen = (num1) => {
+const binToDec = (num1) => {
   let arrNum = num1.split("").reverse();
   let res = 0;
-  for (let i = 0; i < arrNum.length; i++) {
-    if (arrNum[i] > 1) {
-      return "Не корректный ввод";
-    }
-    if (arrNum[i] == 1) {
-      res += 2 ** i;
-    }
-  }
+
+  arrNum.forEach(function (elem, index) {
+    if (elem > 1) return "Не корректный ввод";
+    if (elem == 1) res += 2 ** index;
+  });
+
   return res;
 };
 
+// Из десятичной
 
-const tenToTwo = (num2) => {
+const decToBin = (num2) => {
   let arrRes = [];
   let num = num2;
+
   while (num !== 0) {
     arrRes.push(num % 2);
     num = Math.floor(num / 2);
   }
+
   return arrRes.reverse().join("");
 };
 
-  
-//  Вторая версия tenToTwo , с применением рекурсии.
+// Из десятичной второй вариант, с применением рекурсии.
 
 let res = [];
-  const tr = num1 => {
-    if ( num1 == 0) {
-      return res;
-    }
-    res.push(num1 % 2);
-    return tr (Math.floor(num1 / 2));
-  }
+const rec = (num1) => {
+  if (num1 == 0) return res.reverse().join("");
 
-console.log( tr(14));
+  res.push(num1 % 2);
+  return rec(Math.floor(num1 / 2));
+};
